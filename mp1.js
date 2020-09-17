0edc36526f2fffb34a7dca9861f7acd949925802
Jaideep Bhat <jaideep.bhat32@gmail.com>
2020-09-16 23:27:49 -0500

    Completed mp1

--- mp1.js
+++ mp1.js
@@ -28,9 +28,14 @@ var mvMatrix = glMatrix.mat4.create();
 /** @global A second ModelView matrix contains any modeling and viewing transformations */
 var mvMatrix2 = glMatrix.mat4.create();
 
+/** @global Getting the current time in seconds */
+var currTime;
 
 /** @global Records time last time frame rendered */
 var previousTime = 0;
+
+/** @global time diff between previous time and now */
+var deltaTime;
 
 /**
  * Function for translating degrees to radians
@@ -119,13 +124,14 @@ function setupShaders() {
 }
 
 /**
- * Populate buffers with data
+ * Function to handle creation of buffers and animation for blockI i.e. the first animation
  */
-function setupBuffers() {
+function drawBlockI() {
+  
   vertexPositionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
   var triangleVertices = [
-         -0.6, 0.9, 0.0,
+        -0.6, 0.9, 0.0,
         -0.6, 0.6, 0.0,
         -0.2, 0.6, 0.0,
         -0.6, 0.9, 0.0,
@@ -239,12 +245,22 @@ function setupBuffers() {
          0.6, 0.6, 0.0,
          0.7, 0.9, 0.0,
          0.6, 0.9, 0.0
-         
-        
+                
   ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+  for (var i = 0; i < triangleVertices.length; i += 3) {
+      if (currTime % 60 < 20) {
+          triangleVertices[i] *= Math.cos(degToRad(document.getElementById("degrees").value));
+          triangleVertices[i+1] *= 2;
+      } else if (currTime % 60 > 20) {
+          triangleVertices[i] *= 2;
+          triangleVertices[i+1] *= Math.cos(degToRad(document.getElementById("degrees").value));
+      } else {
+          triangleVertices[i] *= 1;
+      }
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.DYNAMIC_DRAW);
   vertexPositionBuffer.itemSize = 3;
-  vertexPositionBuffer.numberOfItems = 114;
+  vertexPositionBuffer.numberOfItems = 114;   
     
   vertexColorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
@@ -252,7 +268,6 @@ function setupBuffers() {
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
-         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
@@ -279,6 +294,7 @@ function setupBuffers() {
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
         0.9, 0.29, 0.15, 1.0,
+        0.9, 0.29, 0.15, 1.0,
         0.07, 0.16, 0.29, 1.0,
         0.07, 0.16, 0.29, 1.0,
         0.07, 0.16, 0.29, 1.0,
@@ -362,20 +378,12 @@ function setupBuffers() {
         0.07, 0.16, 0.29, 1.0,
         0.07, 0.16, 0.29, 1.0,
         0.07, 0.16, 0.29, 1.0,
-        0.07, 0.16, 0.29, 1.0,
-        
-        
+        0.07, 0.16, 0.29, 1.0,   
     ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
   vertexColorBuffer.itemSize = 4;
   vertexColorBuffer.numItems = 114;  
     
-}
-
-/**
- * Draw model...render a frame
- */
-function draw() { 
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight); 
   gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -393,38 +401,111 @@ function draw() {
     
   // Render I
   gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+    
+  
+  var scaler = document.getElementById("scale").value;
+   // Scalar vector used for translation
+  var scaleVec = glMatrix.vec3.fromValues(scaler, scaler, 0);
+
+  
+  // Update geometry to rotate speed degrees per second
+  // Also Update Geometry to change scale of I
+     
+     glMatrix.mat4.fromScaling(mvMatrix2, scaleVec);
+     glMatrix.mat4.fromZRotation(mvMatrix, degToRad(rotAngle));
+}
+
+/**
+ * Function to handle creation of buffers and animation
+ */
+function drawCustomAnimation() { 
+    
+  vertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  var triangleVertices = [
+      0.0, 0.0, 0.0,
+      -0.3, 1.0, 0.0,
+      0.3, 1.0, 0.0,
+      0.0, 0.0, 0.0,
+      -0.3, -1.0, 0.0,
+      -0.5, -0.8, 0.0,
+      0.0, 0.0, 0.0,
+      0.3, -1.0, 0.0,
+      0.5, -0.8, 0.0,
+      
+  ];
+    // Math operation to multiply vertex coordinates by sine value to reverse coordinates
+  for (var i = 0; i < triangleVertices.length; i++) {
+      triangleVertices[i] = triangleVertices[i] * Math.sin(degToRad(document.getElementById("degrees").value));
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.DYNAMIC_DRAW);
+  vertexPositionBuffer.itemSize = 3;
+  vertexPositionBuffer.numberOfItems = 9;
+    
+  vertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  var colors = [
+     0.07, 0.16, 0.29, 1.0,
+     0.9, 0.29, 0.15, 1.0,
+     0.69, 0.98, 0.01, 1.0,
+     0.07, 0.16, 0.29, 1.0,
+     0.9, 0.29, 0.15, 1.0,
+     0.69, 0.98, 0.01, 1.0,
+     0.07, 0.16, 0.29, 1.0,
+     0.9, 0.29, 0.15, 1.0,
+     0.69, 0.98, 0.01, 1.0,
+    ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.DYNAMIC_DRAW);
+  vertexColorBuffer.itemSize = 4;
+  vertexColorBuffer.numItems = 9;  
+    
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight); 
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+                         vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
+                            vertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    
+  // Send current ModelView matrix to vertex shader
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+    
+  // Render I
+  gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+
+  // Update geometry to rotate
+  glMatrix.mat4.fromZRotation(mvMatrix, degToRad(rotAngle));
+  
 }
 
 /**
  * Function that updates geometry and repeatedly renders frames.
  */
  function animate(now) {
+     
+      // Convert time to seconds
      var speed = document.getElementById("speed").value;
-     var scaler = document.getElementById("scale").value;
-     // Scalar vector used for translation
-     var scaleVec = glMatrix.vec3.fromValues(scaler, scaler, 0);
-
-     draw();
-     
-     // Convert time to seconds
-     now *= 0.001;
-     // Subtract previous time from current time
-     var deltaTime = now - previousTime;
-     // Remember current time for next frame
-     previousTime = now;
-     
-     // Update geometry to rotate speed degrees per second
+      now *= 0.001;
+       // Subtract previous time from current time
+      deltaTime = now - previousTime;
+       // Remember current time for next frame
+      previousTime = now;
+      currTime = now;
      rotAngle += speed*deltaTime;
      if (rotAngle > 360.0) {
          rotAngle = 0.0;
      }
-     glMatrix.mat4.fromScaling(mvMatrix2, scaleVec);
-     glMatrix.mat4.fromZRotation(mvMatrix, degToRad(rotAngle));
-     
+     if (document.getElementById("animationA").checked) {
+         drawBlockI();
+     } else {
+         drawCustomAnimation();
+     } 
      
      // ...next frame after
-     requestAnimationFrame(animate);
-     
+     requestAnimationFrame(animate);  
      
  }
 
@@ -437,7 +518,6 @@ function draw() {
   canvas = document.getElementById("myGLCanvas");
   gl = createGLContext(canvas);
   setupShaders(); 
-  setupBuffers();
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   requestAnimationFrame(animate);  
 }
